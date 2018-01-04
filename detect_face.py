80a3766fb86c66e654dae20ccfe03be86cd40557
tespin <tristan.m.espinoza@gmail.com>
2018-01-04 18:07:49 +0800

    Testing facial landmarking.

--- detect_face.py
+++ detect_face.py
@@ -1,6 +1,8 @@
 import numpy as np
 import argparse
 from process import resize
+from process import shape_to_numpy
+from process import rect_to_bounding
 import dlib
 import cv2
 
@@ -19,3 +21,16 @@ image = resize(image, width=500)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 rects = detector(gray, 1)
+
+for (index, rect) in enumerate(rects):
+    shape = predictor(gray, rect)
+    shape = shape_to_numpy(shape)
+
+    (x, y, w, h) = rect_to_bounding(rect)
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    for (x, y) in shape:
+        cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+
+cv2.imshow("Output", image)
+cv2.waitKey(0)
