9a444b04e2d45596473b3bb478f6312db3bd607d
tespin <tristan.m.espinoza@gmail.com>
2018-01-07 17:01:37 +0800

    Trying to clean up mess.

--- recognize_dwayne.py
+++ recognize_dwayne.py
@@ -3,6 +3,7 @@ import numpy as np
 from process import resize
 from process import shape_to_numpy
 from process import rect_to_bounding
+from face_aligner = import FaceAligner
 import dlib
 import cv2
 
@@ -19,6 +20,7 @@ images = []
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(args["shape_predictor"])
 recognizer = dlib.face_recognition_model_v1(args["recognition_model"])
+aligner = FaceAligner(predictor, desiredFaceWidth=256)
 
 imDwaynePath = os.path.basename(args["dwayne"])
 imUnknownPath = os.path.basename(args["unknown"])
@@ -41,7 +43,8 @@ for path, image in images.items():
     rects = detector(gray, 1)
 
     if len(rects) is 0:
-        print("[INFO] No faces found in image {}! Skipping...".format(path))
+        print("[INFO] No faces found in image {}! Please suppy an image with a detected face. Exiting...".format(path))
+        exit()
     else:
         rois = []
 
@@ -49,9 +52,45 @@ for path, image in images.items():
             shape = predictor(gray, rect)
 
             face_descriptor = recognizer.compute_face_descriptor(image, shape)
+            encodings.extend(face_descriptor)
             print("[INFO] Face descriptor for image {}.".format(path))
+            shape = shape_to_numpy(shape)
 
+            (x, y, w, h) = rect_to_bounding(rect)
 
+            if y < 0:
+                y = 0
+            elif y > y + h:
+                y = y + h
+
+            if x < 0:
+                x = 0
+            elif x > x + w:
+                x = x + w
+
+            roi = image[y + 1:y + h, x + 1:x + w]
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 1)
+            roiAligned = aligner.align(image, gray, rect)
+            roiAligned = resize(roiAligned, width=256)
+            rois.append(roiAligned)
+
+            for x, y in shape:
+                cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+
+            roi = resize(roi, width=256)
+            rois.append(roi)
+
+        if encodings[0] - encodings[1] < 0.6:
+            print("Unknown image {} is Dwayne!".format(path))
+        else:
+            print("That's not The Rock!")
+
+        for index, region in enumerate(rois):
+            cv2.imshow("Face {}".format(index + 1), region)
+
+cv2.imshow("Dwayne The Rock Johnson", imDwayne)
+cv2.imshow("Unknown person", imUnknown)
+cv2.waitKey(0)
 
 #for index, image in enumerate(images):
 #    image = resize(image, width=500)
