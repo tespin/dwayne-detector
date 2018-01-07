4c269eca0c7ff409027806c5984d7effc41d9244
tespin <tristan.m.espinoza@gmail.com>
2018-01-07 16:39:58 +0800

    Testing use of dict to hold paths and images.

--- recognize_dwayne.py
+++ recognize_dwayne.py
@@ -20,18 +20,57 @@ detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(args["shape_predictor"])
 recognizer = dlib.face_recognition_model_v1(args["recognition_model"])
 
-imDwayne = cv2.imread(args["dwayne"])
-imUnknown = cv2.imread(args["unknown"])
-images.extend([imDwayne, imUnknown])
+imDwaynePath = os.path.basename(args["dwayne"])
+imUnknownPath = os.path.basename(args["unknown"])
+
+#imDwayne = cv2.imread(args["dwayne"])
+#imUnknown = cv2.imread(args["unknown"])
+#images.extend([imDwayne, imUnknown])
+
+imDwayne = cv2.imread(imDwaynePath)
+imUnknown = cv2.imread(imUnknownPath)
+
+images = {imDwaynePath: imDwayne, imUnknownPath: imUnknown}
 
 encodings = []
 
-for image in images:
+for path, image in images.items():
     image = resize(image, width=500)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    rects = detectory(gray, 1)
 
+    rects = detector(gray, 1)
+
+    if len(rects) is 0:
+        print("[INFO] No faces found in image {}! Skipping...".format(path))
+    else:
+        rois = []
+
+        for rect in rects:
+            shape = predictor(gray, rect)
+
+            face_descriptor = recognizer.compute_face_descriptor(image, shape)
+            print("[INFO] Face descriptor for image {}.".format(path))
+
+
+
+#for index, image in enumerate(images):
+#    image = resize(image, width=500)
+#    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#    
+#    rects = detector(gray, 1)
+#
+#    if len(rects) is 0:
+#        print("[INFO] No faces found in image {}! Skipping...".format(index))
+#        continue
+#    else:
+#        rois = []
+#
+#        for rect in rects:
+#            shape = predictor(gray, rect)
+#
+#            face_descriptor = recognizer.compute_face_descriptor(image, shape)
+#            print("[INFO]Face descriptor for image{}
+#
 # take two images as input
 # detect face
 # align face
