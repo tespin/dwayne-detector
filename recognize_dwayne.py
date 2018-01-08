53ea00bd5c3a364888e06ab132a64880c54178cc
tespin <tristan.m.espinoza@gmail.com>
2018-01-08 11:04:56 +0800

    Cleaning up to see if it helps.

--- recognize_dwayne.py
+++ recognize_dwayne.py
@@ -30,44 +30,13 @@ imDwayne = cv2.imread(args["dwayne"])
 # define other image with another detectable face
 imUnknown = cv2.imread(args["unknown"])
 
-images.extend([imDwayne, imUnknown])
+images.extend([args["dwayne"], args["unknown"]])
 
 encodings = []
 
-# process each face
-for index, image in enumerate(images):
+for path in images:
+    image = cv2.imread(path)
     image = resize(image, width=500)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Input {}".format(path), image)
 
-    rects = detector(gray, 1)
-
-    if len(rects) is 0:
-        print("No faces found in this image! Please supply an image with a detected face. Exiting...")
-        exit()
-
-    else:
-        rois = []
-
-        for rect in rects:
-            shape = predictor(gray, rect)
-            face_descriptor = recognizer.compute_face_descriptor(image, shape)
-            print("Face Descriptor for Image {}: {}".format(index, face_descriptor))
-            encodings.extend(face_descriptor)
-            shape = shape_to_numpy(shape)
-
-#    for encoding in encodings:
-#        print("Encodings for image {}: {}".format(index, encoding))
-
-#vector = encodings[0] - encodings[1]
-#print("Distance: {}".format(distance))
-#print(sqrt(np.dot(encodings[0], encodings[1])))
-#print("Dot product: {}".format(np.dot(vector, vector)))
-#print("Length: {}".format(sqrt(np.dot(vector, vector))))
-
-distance = np.linalg.norm(encodings[0] - encodings[1])
-print("Distance: {}".format(distance))
-
-# compute face_descriptor for dwayne
-# compute face_descriptor for other input
-# subtrct dlib vectors to find euclidean distance
-# if < 0.6, it is dwayne!
+cv2.waitKey(0)
