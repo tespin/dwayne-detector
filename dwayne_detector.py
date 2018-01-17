e1c9a517499f1a6ccacc68a6191c2d336f81a40f
tespin <tristan.m.espinoza@gmail.com>
2018-01-17 08:57:37 -0800

    Issues with locations, nonetypes...

--- dwayne_detector.py
+++ dwayne_detector.py
@@ -9,11 +9,16 @@ detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 recognizer = dlib.face_recognition_model_v1("dlib_face_recognition_resnet_model_v1.dat")
 
+def rect_to_tuple(rect):
+    return rect.top(), rect.right(), rect.bottom(), rect.left()
+
 def raw_locations(input, upsample=1):
     return detector(input, upsample)
 
 def face_locations(input, upsample=1):
+    input = resize(input, width=400)
+
     for location in raw_locations(input, upsample):
-        return [rect_to_bounding(location)]
+        return [rect_to_tuple(location)]
 
 
