eb313a5042a1cbf545e769ad80c48a41c98d60b1
tespin <tristan.m.espinoza@gmail.com>
2018-01-22 15:49:20 -0800

    Updating for landmarks with multiple faces.

--- dwayne_detector.py
+++ dwayne_detector.py
@@ -2,11 +2,11 @@ import numpy as np
 from process import resize
 from process import shape_to_numpy
 from process import rect_to_bounding
+from process import bounding_to_rect
 import dlib
 import cv2
 
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 recognizer = dlib.face_recognition_model_v1("dlib_face_recognition_resnet_model_v1.dat")
 
 def rect_to_tuple(rect):
@@ -22,3 +22,28 @@ def face_locations(input, upsample=1):
         #return [rect_to_bounding(location)]
 
     return [rect_to_bounding(location) for location in raw_locations(input, upsample)]
+
+def raw_landmarks(input, locations = None):
+    if locations is None:
+        locations = raw_locations(input)
+    else:
+        locations = [bounding_to_rect(location) for location in locations]
+
+
+    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+    return [predictor(input, location) for location in locations]
+
+def face_encodings(input, locations=None, num_jitters=1):
+    landmarks = raw_landmarks(input, locations)
+
+    return [np.array(recognizer.compute_face_descriptor(input, landmark, num_jitters)) for landmark in landmarks]
+
+def face_distance(encodings, face_input):
+    if len(face_encodings) == 0:
+        return np.empty((0))
+
+    return np.linalg.norm(encodings - face_input)
+
+def compare(known_encoding, unknown_encoding, tolerance=0.6):
+    return list (face_distance(known_encoding, unknown_encoding) <= tolerance)
