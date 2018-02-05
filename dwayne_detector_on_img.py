dd2664386291cdff88458b0c2c1e1ed9d2cc78fb
tespin <tristan.m.espinoza@gmail.com>
2018-02-04 21:26:29 -0800

    Detecting matches.

--- dwayne_detector_on_img.py
+++ dwayne_detector_on_img.py
@@ -15,5 +15,21 @@ unknown = cv2.imread(args["unknown"])
 unknown = dwayne_detector.resize(unknown, width=800)
 
 dwayne_encoding = dwayne_detector.face_encodings(dwayne)[0]
+
+unknown_locations = dwayne_detector.face_locations(unknown)
 unknown_encodings = dwayne_detector.face_encodings(unknown)
+
+for (x, y, w, h), unknown_encoding in zip(unknown_locations, unknown_encodings):
+    result = dwayne_detector.compare(dwayne_encoding, unknown_encoding)
+    print(result)
+
+    name = "Not Dwayne"
+
+    if True in result:
+        name = "Dwayne"
+
+    cv2.rectangle(unknown, (x, y), (x + w, y + h), (0, 255, 0), 1)
+    cv2.rectangle(unknown, (x, y + h), (x + w, y + h + 30), (0, 255, 0), cv2.FILLED)
+    cv2.putText(unknown, name, (x + 6, y + h + 25), cv2.FONT_HERHSEY_DUPLEX, 1.0, (255, 255, 255), 1)
+
 
