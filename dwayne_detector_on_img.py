16e9c3162b3f2c3f4782b53348961128db6de25e
tespin <tristan.m.espinoza@gmail.com>
2018-02-04 21:20:04 -0800

    Loading imgs for Dwayne and unknown.

--- dwayne_detector_on_img.py
+++ dwayne_detector_on_img.py
@@ -2,3 +2,14 @@ import argparse
 import dwayne_detector
 import dlib
 import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dwayne", required=True, help="path to Dwayne")
+ap.add_argument("-u", "--unknown", required=True, help="path to img of unknown person")
+args = vars(ap.parse_args())
+
+dwayne = cv2.imread(args["dwayne"])
+dwayne = dwayne_detector.resize(dwayne, width=800)
+
+unknown = cv2.imread(args["unknown"])
+unknown = dwayne_detector.resize(unknown, width=800)
