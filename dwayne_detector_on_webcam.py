f5ab443f5d55714e40b81c218eeae38ad7838189
tespin <tristan.m.espinoza@gmail.com>
2018-02-05 22:54:13 -0800

    Loading image of Dwayne.

--- dwayne_detector_on_webcam.py
+++ dwayne_detector_on_webcam.py
@@ -6,3 +6,6 @@ import cv2
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dwayne", required=True, help="path to Dwayne")
 args = vars(ap.parse_args())
+
+dwayne = cv2.imread(args["unknown"])
+dwayne = dwayne_detector.resize(dwayne, width=800)
