187ec59cc85b4747808957c076dffb0b23e59b6f
tespin <tristan.m.espinoza@gmail.com>
2018-02-05 23:33:43 -0800

    Displaying video.

--- dwayne_detector_on_webcam.py
+++ dwayne_detector_on_webcam.py
@@ -33,3 +33,12 @@ while True:
        cv2.rectangle(unknown, (x, y), (x + w, y + h), (0, 255, 0), 1)
        cv2.rectangle(unknown, (x, y + h), (x + w, y + h + 30), (0, 255, 0), cv2.Filled)       
        cv2.putText(unknown, name, (x + 6, y + h + 25), cv2.FONT_HERSHEY_DUPLEX, 1.0, (255, 255, 255), 1)
+
+    cv2.imshow("Video", frame)
+    key = cvw.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+capture.release()
